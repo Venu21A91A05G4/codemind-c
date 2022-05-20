e97fcc7d8674332df185bae417abc4430917d91c
Venu21A91A05G4 <97424426+Venu21A91A05G4@users.noreply.github.com>
2022-05-20 20:23:26 +0530

    #include<stdio.h> int main() {     int n,a[100],min=-1000,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(min<a[i])         min=a[i];     }     printf("%d",min); }

---
+++ maximum_of_an_array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],min=-1000,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(min<a[i])
+        min=a[i];
+    }
+    printf("%d",min);
+}
