28f0ca0dcc090a3621b847e38d7503580235dd19
Venu21A91A05G4 <97424426+Venu21A91A05G4@users.noreply.github.com>
2022-05-20 06:56:19 +0530

    #include<stdio.h> int main() {     int a,b,hcf,min,i;     scanf("%d%d",&a,&b);     min=(a<b)?a:b;     for(i=1;i<=min;i++)     {         if(a%i==0 && b%i==0)         {             hcf=i;         }     }     printf("%d",hcf); }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,hcf,min,i;
+    scanf("%d%d",&a,&b);
+    min=(a<b)?a:b;
+    for(i=1;i<=min;i++)
+    {
+        if(a%i==0 && b%i==0)
+        {
+            hcf=i;
+        }
+    }
+    printf("%d",hcf);
+}
