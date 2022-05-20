c790753d0314808fbbfeb8a8e52df21aa4613548
Venu21A91A05G4 <97424426+Venu21A91A05G4@users.noreply.github.com>
2022-05-20 18:42:18 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,d,s=0,c,f=0,temp;     scanf("%d",&n);     temp=n;     while(n)     {         d=n%10;         f++;         n/=10;     }     n=temp;     while(temp)     {         d=temp%10;         s+=pow(d,f);         f--;         temp/=10;     }     if(s==n)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Disarium_number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,d,s=0,c,f=0,temp;
+    scanf("%d",&n);
+    temp=n;
+    while(n)
+    {
+        d=n%10;
+        f++;
+        n/=10;
+    }
+    n=temp;
+    while(temp)
+    {
+        d=temp%10;
+        s+=pow(d,f);
+        f--;
+        temp/=10;
+    }
+    if(s==n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
