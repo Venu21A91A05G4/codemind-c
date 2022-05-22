7853614ade14fc9f845ba4c9fd9c2c4c39f50750
Venu21A91A05G4 <97424426+Venu21A91A05G4@users.noreply.github.com>
2022-05-22 09:26:04 +0530

    #include<stdio.h> int main() { 	int a[10][10],es=0,os=0,c,i,j,r;     scanf("%d%d",&r,&c); 	for(i=0;i<r;i++) 	{ 		for(j=0;j<c;j++) 		{ 			scanf("%d",&a[i][j]); 		} 	} 	for(i=0;i<r;i++) 	{ 		for(j=0;j<c;j++) 		{ 		   if(a[i][j]%2) 		   os+=a[i][j]; 		   else 		   es+=a[i][j]; 		} 	} 	printf("%d %d",es,os);
    }

---
+++ Sum_of_even_and__odd_elements_in_a_matrix.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int a[10][10],es=0,os=0,c,i,j,r;
+    scanf("%d%d",&r,&c);
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+		   if(a[i][j]%2)
+		   os+=a[i][j];
+		   else
+		   es+=a[i][j];
+		}
+	}
+	printf("%d %d",es,os);
+	
+
+}
