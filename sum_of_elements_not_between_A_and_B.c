35f72d61b261ebed2070ac8d4771708bfc38649e
Venu21A91A05G4 <97424426+Venu21A91A05G4@users.noreply.github.com>
2022-05-22 07:47:05 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,a,b,s[100],i,c=0,k=0;     scanf("%d",&n);     for(i=1;i<=n;i++)         scanf("%d",&s[i]);     scanf("%d%d",&a,&b);     for(i=1;i<=n;i++)        {            if(s[i]<a)            c+=s[i];            else if(s[i]>b)            c+=s[i];        }     printf("%d",c); }

---
+++ sum_of_elements_not_between_A_and_B.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,a,b,s[100],i,c=0,k=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+        scanf("%d",&s[i]);
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=n;i++)
+       {
+           if(s[i]<a)
+           c+=s[i];
+           else if(s[i]>b)
+           c+=s[i];
+       }
+    printf("%d",c);
+}
